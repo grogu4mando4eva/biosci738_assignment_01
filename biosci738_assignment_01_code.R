3a243e78818fd949fe08c30fb3598add1118fafb
grogu4mando4eva <haileighp@rocketmail.com>
2022-03-15 14:34:10 +1300

    push again

--- biosci738_assignment_01_code.R
+++ biosci738_assignment_01_code.R
@@ -1,5 +1,13 @@
 #load dataset into R
 require(tidyverse)
+require(dplyr)
 library(readr)
 url <- "https://raw.githubusercontent.com/STATS-UOA/databunker/master/data/dicots_proportions.csv"
 data <- read_csv(url)
+
+#extract the calluna data from the dataset
+callunavulgaris_data <- data %>% select(starts_with("Calluna"), `Treat!`)
+
+#save dataframe in project folder
+save(callunavulgaris_data, file = "callunavulgaris_data.RData")
+
