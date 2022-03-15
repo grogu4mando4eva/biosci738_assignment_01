f9372935fbd3ab36049d3d3973dbe92c3fcc0b8a
grogu4mando4eva <haileighp@rocketmail.com>
2022-03-15 18:56:02 +1300

    Commit new code

--- biosci738_assignment_01_code.R
+++ biosci738_assignment_01_code.R
@@ -11,4 +11,15 @@ callunavulgaris_data <- data %>% select(starts_with("Calluna"), `Treat!`)
 #save dataframe in project folder
 save(callunavulgaris_data, file = "callunavulgaris_data.RData")
 
+## rename columns to match the dataset 
+names(callunavulgaris_data)[1] <- '2008'
+names(callunavulgaris_data)[2] <- '2009'
+names(callunavulgaris_data)[3] <- '2010'
+names(callunavulgaris_data)[4] <- '2012'
+names(callunavulgaris_data)[5] <- 'Treatment'
 
+#extrapolating the data sets based on treatment type
+callunavulgaris_treatment_B <- subset(callunavulgaris_data, Treatment=="B")
+callunavulgaris_treatment_C <- subset(callunavulgaris_data, Treatment=="C")
+callunavulgaris_treatment_HB <- subset(callunavulgaris_data, Treatment=="HB")
+callunavulgaris_treatment_H <- subset(callunavulgaris_data, Treatment=="H")
